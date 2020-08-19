9275bb4d1d21a414187015c3f3daca433d632ce9
Gayeon <gayeon0090990@gmail.com>
2020-08-18 19:15:18 -0700

    python1

---
+++ python1.py
@@ -0,0 +1,19 @@
+x = ['ha','do','uo']
+y = 7
+z = 10
+a = 'bamboo boo ba bo bamm be bou bo be'
+b = 'oh '
+c = 'wow!!!!!'
+d = ' fantastic!!'
+print('jo' not in x)
+print('ha' in x)
+print(not(y>5 and y<8))
+print(y>=z)
+print(y % z)
+print(a)
+print(a.strip())
+print(a.upper())
+print(a[3:7])
+print(len(a))
+print(b + c + d)
+
